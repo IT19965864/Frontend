d5f0bd6c2e6385b99948539e4bf9b24f20d84ae5
malindujethaka <malindujethaka@gmail.com>
2022-03-19 15:30:37 +0530

    create pages

---
+++ src/pages/AddStudent.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import Navbar from '../components/StuNavBar';
+import '../styles/student.css';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+
+import * as Yup from 'yup';
+
+
+
+
+function AddStudent() {
+
+  const formik=useFormik({
+    initialValues:{
+      stuName:'',
+      nic:'',
+      email:'',
+      mobile:'',
+      grade:''
+  
+    },
+  
+    validationSchema:Yup.object({
+      stuName:Yup.string()
+              .min(3,"?")
+              .max(30,"Input 30 characters or below")
+              .matches(/^[A-Za-z ]*$/,"Please Enter Valid name")
+              .required("Required")
+    })
+  
+  })
+  
+  return (
+    <>
+        <Navbar/>
+        <div>
+          
+        <Form id='student-form' onSubmit={formik.handleSubmit}>
+        <label id ='student-form-label'>Add  Student</label>
+          <Form.Field>
+            <label>Student Name</label>
+            <input
+             placeholder='Student Name'
+             type="text"
+             onChange={formik.handleChange}
+             onBlur={formik.handleBlur}
+             value={formik.values.stuName}
+              />
+            {formik.touched.stuName && formik.errors.stuName ? (
+              <div style={{color: "red"}}>{formik.errors.stuName}</div>
+            ) : null}
+
+            <br/>
+          </Form.Field>
+         
+          <Form.Field>
+            <label>NIC</label>
+            <input placeholder='NIC' 
+            type="text"
+           
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Email</label>
+            <input placeholder='Email' 
+            type="text"
+           
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Mobile</label>
+            <input placeholder='Mobile'
+            type="number" 
+           
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Grade</label>
+            <input placeholder='Grade'
+            type="text"
+           />
+          </Form.Field>
+         
+          <Button primary type='submit' size='small'>Submit</Button>
+          <Button secondary type='clear' size='small'>Reset</Button>
+        </Form>
+        </div>
+        
+       
+    </>
+   
+  );
+  
+  
+  
+  
+}
+
+export default AddStudent
---
+++ src/pages/ViewStudent.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { Table,Button,Icon } from 'semantic-ui-react'
+import Navbar from '../components/StuNavBar'
+import '../styles/student.css';
+
+const colors = [
+  
+  'blue',
+ 
+]
+function ViewStudent() {
+  return (
+    <div>
+       <Navbar/>
+      <div className='student'>
+      {colors.map((color) => (
+        <Table striped id='student-table'color={color} key={color}>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>NIC</Table.HeaderCell>
+              <Table.HeaderCell>Gender</Table.HeaderCell>
+              <Table.HeaderCell>Address</Table.HeaderCell>
+              <Table.HeaderCell>Email</Table.HeaderCell>
+              <Table.HeaderCell>Contact</Table.HeaderCell>
+              <Table.HeaderCell>Action</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>John Lilki</Table.Cell>
+              <Table.Cell>199835712658</Table.Cell>
+              <Table.Cell>Male</Table.Cell>
+              <Table.Cell>no 24,malabe</Table.Cell>
+              <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
+              <Table.Cell>0767968545</Table.Cell>
+              <Table.Cell><Button secondary type='viewmore' size='small'>View More</Button></Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Jamie Harington</Table.Cell>
+              <Table.Cell>199835712696</Table.Cell>
+              <Table.Cell>Male</Table.Cell>
+              <Table.Cell>no 35,malabe</Table.Cell>
+              <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
+              <Table.Cell>0767985245</Table.Cell>
+              <Table.Cell><Button secondary type='viewmore' size='small'>View More</Button></Table.Cell>
+            </Table.Row>
+          </Table.Body>
+          <Table.Footer fullWidth>
+            <Table.Row>
+              <Table.HeaderCell />
+              <Table.HeaderCell colSpan='6'>
+                <Button
+                  floated='right'
+                  icon
+                  labelPosition='left'
+                  primary
+                  size='small'
+                >
+                  <Icon name='file pdf' /> Generate Report
+                </Button>
+               
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Footer>
+        </Table>
+      ))}
+      </div>
+    </div>
+  )
+}
+
+export default ViewStudent
