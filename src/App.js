49e00d06b5b60c57ad03e894470d271dbd0b6607
malindujethaka <malindujethaka@gmail.com>
2022-03-19 15:33:42 +0530

    upldated frontend files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="App">Sajana abertahna pamkaya</div>;
-}
+import './App.css';
+import React, { Suspense, lazy } from 'react';
+import{BrowserRouter as Router,Route, Routes} from 'react-router-dom'
+const StuAdminProfile = lazy(() => import('./pages/StuAdminProfile'));
+const AddStudent = lazy(() => import('./pages/AddStudent'));
+const ViewStudent = lazy(() => import('./pages/ViewStudent'));
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/stuAdminProfile" element={< StuAdminProfile />} />
+        <Route path="/addStudent" element={<AddStudent />} />
+        <Route path="/viewStudent" element={<ViewStudent />} />
+      
+      </Routes>
+    </Suspense>
+  </Router>
+);
+
 
 export default App;
