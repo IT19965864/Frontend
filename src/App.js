5d996417a6f1873541e47235fc6802a53449c547
menaka809 <malithjayasekara2255721@gmail.com>
2022-03-24 23:43:16 +0530

    added add teacher route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import React, { Suspense, lazy } from 'react';
-import{BrowserRouter as Router,Route, Routes} from 'react-router-dom'
-const StuAdminProfile = lazy(() => import('./pages/StuAdminProfile'));
-const AddStudent = lazy(() => import('./pages/AddStudent'));
-const ViewStudent = lazy(() => import('./pages/ViewStudent'));
+import "./App.css";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+const StuAdminProfile = lazy(() => import("./pages/StuAdminProfile"));
+const AddStudent = lazy(() => import("./pages/AddStudent"));
+const ViewStudent = lazy(() => import("./pages/ViewStudent"));
+const AddTeacher = lazy(() => import("./pages/AddTeacher"));
 const App = () => (
   <Router>
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/stuAdminProfile" element={< StuAdminProfile />} />
+        <Route path="/stuAdminProfile" element={<StuAdminProfile />} />
         <Route path="/addStudent" element={<AddStudent />} />
         <Route path="/viewStudent" element={<ViewStudent />} />
-      
+        <Route path="/addTeacher" element={<AddTeacher />} />
       </Routes>
     </Suspense>
   </Router>
 );
-
 
 export default App;
