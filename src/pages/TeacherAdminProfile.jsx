dcd67f5388936e102741660c988cc1930654e021
menaka809 <malithjayasekara2255721@gmail.com>
2022-05-08 18:13:47 +0530

    created teacher admin profile

--- src/pages/TeacherAdminProfile.jsx
+++ src/pages/TeacherAdminProfile.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import Navbar from "../components/StuNavBar";
+import Navbar from "../components/TeacherNavBar";
 import "../styles/student.css";
+import { Card, Icon, Image } from "semantic-ui-react";
+
 function StuAdminProfile() {
   return (
-    <div>
+    <>
       <Navbar />
-      <div className="student">
-        <h1>student admin profile</h1>
-      </div>
-    </div>
+
+      <Card id="admincard">
+        <Image
+          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>Malith</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2022</span>
+          </Card.Meta>
+          <Card.Description>Email:malithmenaka@gmail.com</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name="user" />
+            Teacher Admin
+          </a>
+        </Card.Content>
+      </Card>
+    </>
   );
 }
 
