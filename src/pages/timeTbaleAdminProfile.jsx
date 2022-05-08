66ffffb7b666020f167b2f5c517656ecb4e6f027
IT19965864 <sajanaran@gmail.com>
2022-05-08 21:28:09 +0530

    setlle logging bug

--- src/pages/timeTbaleAdminProfile.jsx
+++ src/pages/timeTbaleAdminProfile.jsx
@@ -1,7 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
+import Navbar from "../components/TimetableNav";
+import "../styles/student.css";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default class timeTbaleAdminProfile extends Component {
-  render() {
-    return <div></div>;
-  }
+function timeTbaleAdminProfile() {
+  return (
+    <>
+      <Navbar />
+
+      <Card id="admincard">
+        <Image
+          src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>Sajana</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2021</span>
+          </Card.Meta>
+          <Card.Description>Email:sajanaran@gmail.com</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name="user" />
+            Timetable Admin
+          </a>
+        </Card.Content>
+      </Card>
+    </>
+  );
 }
+
+export default timeTbaleAdminProfile;
